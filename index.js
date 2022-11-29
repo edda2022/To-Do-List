d100c1cc31d149657818eb4049d65e1bead5d0ce
edda2022 <candyreinecke@googlemail.com>
2022-11-29 17:54:02 +0100

    adding JS function and array

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
+const tasks = [
+    {
+     id: 1,
+     description: "Delete the task",
+    },
+    {
+    id: 2,
+    description: "Edit the task",
+    },
+    {
+    id: 3,
+    description: "Mark the task as done",
+    },
+];
+
+
+const listSectionFunction = (task) => {
+    const listSection = document.createElement("div")
+    listSection.classList.add("task")
+
+    const listSectionChild = document.createElement("div")
+    listSectionChild.classList.add("taskitem")
+
+    listSectionChild.innerText = task.description;
+
+    const buttonGroup = document.createElement("div")
+    buttonGroup.classList.add("buttons")
+
+    const editButton = document.createElement('button')
+    editButton.classList.add("btn-outline-warning", "btn", "buttons")
+    editButton.innerText = 'Edit'
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add("btn-outline-danger", "btn", "buttons")
+    deleteButton.innerText = 'Delete'
+
+    const doneButton = document.createElement('button')
+    doneButton.classList.add("btn-outline-success", "btn", "buttons")
+    doneButton.innerText = 'Done'
+
+    deleteButton.addEventListener('click', (e) => {
+        listSection.remove();
+    });
+
+    doneButton.addEventListener('click', (e) => {
+        listSectionChild.style.textDecoration = 'line-through'
+    });
+
+    listSection.append(listSectionChild)
+    listSection.append(buttonGroup)
+    buttonGroup.append(editButton)
+    buttonGroup.append(deleteButton)
+    buttonGroup.append(doneButton)
+
+
+
+    return listSection;
+
+    
+};
+
+const displayTaskList = (array) => {
+    const listSectionA = document.querySelector('#list');
+    array.forEach((task) => {
+        const newTask = listSectionFunction(task);
+        listSectionA.appendChild(newTask);
+    });
+};
+
+displayTaskList(tasks);
+
+// Delete a task
+
+
+
 //add to-do-item - Candy
 
 //edit to-do-item - Lukas
